8c817ba713b2a5b55e0ce43f420938e70b011da6
boredcoder411 <taamarcos@gmail.com>
2024-12-06 22:52:40 +0100

    Initial commit

---
+++ src/terminal.js
@@ -0,0 +1,62 @@
+import os from 'os';
+import pty from 'node-pty';
+
+let sharedPtyProcess = null;
+let sharedTerminalMode = false;
+
+const shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
+
+const spawnShell = () => {
+    return pty.spawn(shell, [], {
+        name: 'xterm-256color',
+        env: process.env,
+    });
+};
+
+export const setSharedTerminalMode = (useSharedTerminal) => {
+    sharedTerminalMode = useSharedTerminal;
+    if (sharedTerminalMode && !sharedPtyProcess) {
+        sharedPtyProcess = spawnShell();
+    }
+};
+
+export const handleTerminalConnection = (ws) => {
+    let ptyProcess = sharedTerminalMode ? sharedPtyProcess : spawnShell();
+
+    ws.on('message', command => {
+        const processedCommand = commandProcessor(command);
+        ptyProcess.write(processedCommand);
+    });
+
+    ptyProcess.on('data', (rawOutput) => {
+        const processedOutput = outputProcessor(rawOutput);
+        ws.send(processedOutput);
+    });
+
+    ws.on('close', () => {
+        if (!sharedTerminalMode) {
+            ptyProcess.kill();
+        }
+    });
+};
+
+// Utility function to process commands
+const commandProcessor = (command) => {
+  console.log('commandProcessor', command.toString());
+  command = JSON.parse(command.toString());
+  if (command.event === 'command') {
+    return command.content;
+  } else if (command.event === 'resize') {
+    if (!sharedPtyProcess) {
+      console.warn('Resize event received, but sharedPtyProcess is not initialized.');
+    } else {
+      sharedPtyProcess.resize(command.content.cols, command.content.rows);
+    }
+  }
+  return '';
+};
+
+// Utility function to process output
+const outputProcessor = (output) => {
+    return output;
+};
